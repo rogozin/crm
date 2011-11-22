efb6b243ef71628aa340f7d0afb32685151db009
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-22 13:07:02 +0400

    fixed #2574 - user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 #encoding: utf-8;
-class UsersController < ClientsController
-   before_filter :find_client
-   
+class UsersController < ClientsController   
+  before_filter :find_client, :only => [:index] 
+  before_filter :load_roles, :only => [:edit, :update]
+
   def index
     params[:page] ||= 1
     params[:per_page] ||= 30
@@ -12,6 +13,29 @@ class UsersController < ClientsController
     @users  = User.select("distinct users.*").joins(:role_objects).where("roles.group > 1").order("last_request_at desc, created_at desc").paginate(:page => params[:page], :per_page => params[:per_page])
   end
   
+  
+  def edit
+    @user = User.find(params[:id])
+    @firm = @user.firm
+  end
+  
+  def update
+    @user = User.find(params[:id])    
+    @user.errors[:firm_id] = "такого ID не существует" if params[:user][:firm_id] != @user.firm_id.to_s && !Client.exists?(params[:user][:firm_id])
+    if @user.errors.empty? && @user.update_attributes(params[:user])
+      redirect_to edit_user_path(@user), :notice => "Учетная запись пользователя изменена"
+    else 
+      @firm = @user.firm
+      render :edit
+    end
+  end
+  
+  private
+  
+  def load_roles
+    @roles = Role.where(:group => 2)
+    @hidden_roles = Role.where "`group` <> 2"
+  end
   
 
 
