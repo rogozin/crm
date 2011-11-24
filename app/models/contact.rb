4b08c26a0d7d6b36fd7d455701524a6736eeaa8f
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-24 15:30:47 +0400

    contacts phone validator

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :firm_id, :current_date, :event_id, :contact_type_id
+  validates :phone, :phone => {:allow_blank => true}
   belongs_to :client, :foreign_key => "firm_id"
   belongs_to :event
   belongs_to :contact_type
