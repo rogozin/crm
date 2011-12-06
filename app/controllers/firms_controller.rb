2249855a10b671f2f99c382116b91ec5d3176aaf
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-12-06 11:33:37 +0400

    close #2775 - фильтры в регистрациях. Все 100% регистрации отображаются в crm

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -1,8 +1,17 @@
-class FirmsController < ApplicationController
+class FirmsController < BaseController
   def index
     params[:page] ||=1
     params[:per_page] ||=30
-    @firms = Firm.where("not exists (select null from clients c where c.firm_id = firms.id)").order("created_at desc").paginate(:page => params[:page], :per_page => params[:per_page])
+    params[:sort]||="created_at" 
+    params[:direction]||="desc"
+    @firms = Firm.scoped
+    
+    @firms = @firms.where(:id => params[:id]) if params[:id].present?
+    @firms = @firms.where("short_name like :request or name like :request", {:request => "%#{params[:name]}%"}) if params[:name].present?
+    @firms = @firms.where("phone like :request", {:request => "%#{params[:phone]}%"}) if params[:phone].present?
+    @firms = @firms.where("url like :request", {:request => "%#{params[:site]}%"}) if params[:site].present?
+    @firms = @firms.where("email like :request", {:request => "%#{params[:email]}%"}) if params[:email].present?
+    @firms = @firms.order(order_string).paginate(:page => params[:page], :per_page => params[:per_page])
   end
 
   def show
--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -3,12 +3,12 @@ module ClientsHelper
     firm.short_name.present? ? firm.short_name : firm.name
   end
   
-  def edit_firm_link(firm)
-    link_to_if current_user.is_first_manager? || firm.free? || firm.my?(current_user), firm.name, edit_client_path(firm)  
+  def edit_client_link(firm, text=nil, title=nil)
+    link_to_if current_user.is_first_manager? || firm.free? || firm.my?(current_user), text ? text : firm.name, edit_client_path(firm), :title => title    
   end
   
   def edit_user_link(user)
-    link_to_if  current_user.is_first_manager? ||  (user.client && (user.client.free? || user.client.my?(current_user))), user.username, edit_user_path(user)  
+    link_to_if  current_user.is_first_manager? ||  (user.client && (user.client.free? || user.client.my?(current_user))), user.username, edit_user_path(user)
   end
 
     
